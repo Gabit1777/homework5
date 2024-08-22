19b4fef4464201c3a9cd312d5d311131e8aacc95
Gabit1777 <heroesn@inbox.ru>
2024-08-22 11:58:46 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list = ['apple', 'kiwi', 'orange', 'banana', 'coconut', 'peach', 'pineapple']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[3:5])
+my_list[3] = "watermelon"
+print(my_list)
+my_dict = {'apple': 'яблоkо', "pineapple": 'ананас', "coconut": "какос", "watermelon": "арбуз", "banana": "банан"}
+print(my_dict)
+print(my_dict["watermelon"])
+my_dict["apple"] = "свет"
+print(my_dict)
+my_dict["lightning"] = "гром"
+print(my_dict)
